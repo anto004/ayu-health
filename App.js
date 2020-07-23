d57698291c0fa6606c774d380dccb5f412875518
anto004 <anto_004@live.com>
2020-07-23 12:32:18 +0530

    Add bottom tab navigation with icons

--- App.js
+++ App.js
@@ -1,21 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign, Ionicons, Feather } from "@expo/vector-icons";
+import { Avatar, Text } from "react-native-elements";
+
+function ProfileScreen() {
+	return (
+		<View style={styles.container}>
+			<Text>Profile Component</Text>
+		</View>
+	);
+}
+
+function ActivityScreen() {
+	return (
+		<View style={styles.container}>
+			<Text>Activity Component</Text>
+		</View>
+	);
+}
+
+function AddScreen() {
+	return (
+		<View style={styles.container}>
+			<Text>Add Component</Text>
+		</View>
+	);
+}
+
+function SearchScreen() {
+	return (
+		<View style={styles.container}>
+			<Text>Search Component</Text>
+		</View>
+	);
+}
+
+function HomeScreen() {
+	return (
+		<View style={styles.container}>
+			<Text h1>Open up App.js to start working on your app!</Text>
+		</View>
+	);
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	//<StatusBar style="auto" />
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+
+						if (route.name === "Home") {
+							return focused ? (
+								<Ionicons name="md-home" size={size} color={color} />
+							) : (
+								<AntDesign name="home" size={size} color={color} />
+							);
+						}
+
+						if (route.name === "Search") {
+							return <Ionicons name="md-search" size={size} color={color} />;
+						}
+
+						if (route.name === "Add") {
+							iconName = focused ? "md-add-circle" : "md-add-circle-outline";
+							return <Ionicons name={iconName} size={size} color={color} />;
+						}
+
+						if (route.name === "Activity") {
+							iconName = focused ? "md-heart" : "md-heart-empty";
+							return <Ionicons name={iconName} size={size} color={color} />;
+						}
+
+						if (route.name === "Profile") {
+							//iconName = focused ? "md-heart" : "md-heart-empty";
+							return (
+								<Avatar
+									rounded
+									source={require("./assets/profile-images/profileimage3.jpeg")}
+								/>
+							);
+						}
+					},
+				})}
+				tabBarOptions={{
+					activeTintColor: "black",
+					inactiveTintColor: "gray",
+				}}
+			>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Search" component={SearchScreen} />
+				<Tab.Screen name="Add" component={AddScreen} />
+				<Tab.Screen name="Activity" component={ActivityScreen} />
+				<Tab.Screen name="Profile" component={ProfileScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ data/userdata.js
@@ -0,0 +1,196 @@
+export const DATA = {
+	user1: {
+		id: 1,
+		username: "jonas22",
+		bio:
+			"We all live under the same sky, let's treat each other like we would ourselves🍻",
+		followers: 100,
+		following: 100,
+		posts: [
+			{
+				id: post1,
+				uri: "",
+				liked: 50,
+				postDescription: "Photo of the Day! Top of the world 🙂",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+			{
+				id: post2,
+				uri: "",
+				liked: 50,
+				postDescription: "Photo of the Day! Sunset☀️",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+			{
+				id: post3,
+				uri: "",
+				liked: 50,
+				postDescription: "Photo of the Day! Time in the cabin",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+			{
+				id: post4,
+				uri: "",
+				liked: 50,
+				postDescription: "Photo of the Day! ",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+			{
+				id: post5,
+				uri: "",
+				liked: 50,
+				postDescription: "Photo of the Day! During the protest",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+			{
+				id: post6,
+				uri: "",
+				liked: 50,
+				postDescription: "Photo of the Day! Beautiful Sunset",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+			{
+				id: post7,
+				uri: "",
+				liked: 50,
+				postDescription: "Photo of the Day! Time in the cabin",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+			{
+				id: post8,
+				uri: "",
+				liked: 50,
+				postDescription:
+					"Photo of the Day! Let's meet up here next time",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+			{
+				id: post9,
+				uri: "",
+				liked: 50,
+				postDescription: "Photo of the Day! Evening walk ❤️",
+				comments: [
+					{
+						id: comment1,
+						comment: "Nice View!",
+						commentUserId: 2,
+					},
+					{
+						id: comment2,
+						comment: "Cool!",
+						commentUserId: 3,
+					},
+				],
+			},
+		],
+	},
+	user2: {
+		id: 2,
+		username: "brian76",
+		bio: "",
+		followers: 100,
+		following: 100,
+	},
+	user3: {
+		id: 3,
+		username: "apple34",
+		bio: "",
+		followers: 100,
+		following: 100,
+	},
+	user4: {
+		id: 4,
+		username: "pebs44",
+		bio: "",
+		followers: 100,
+		following: 100,
+	},
+};

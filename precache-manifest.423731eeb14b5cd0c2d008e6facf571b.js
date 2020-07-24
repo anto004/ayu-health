b04e3036fd947bf3798364c2267132209e5d8e15
anto004 <anto_004@live.com>
2020-07-24 16:15:30 +0530

    Updates

---
+++ precache-manifest.423731eeb14b5cd0c2d008e6facf571b.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "8608860797ee0d261f44",
+    "url": "/ayu-health/static/js/app.0ed9bb11.chunk.js"
+  },
+  {
+    "revision": "97e80f740d4b8d937f4a",
+    "url": "/ayu-health/static/js/runtime~app.84fd160e.js"
+  },
+  {
+    "revision": "b20a40d58114c10fb09a",
+    "url": "/ayu-health/static/js/2.a0403deb.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/ayu-health/static/media/star.69c71386.png"
+  },
+  {
+    "revision": "c55c93394d2d400d427e961ed65d0834",
+    "url": "/ayu-health/static/media/profileimage1.394d3189.jpeg"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/ayu-health/static/media/heart.23b5c2c0.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/ayu-health/static/media/rocket.d2f3bf41.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/ayu-health/static/media/bell.cf04af27.png"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/ayu-health/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/ayu-health/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/ayu-health/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/ayu-health/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/ayu-health/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ayu-health/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/ayu-health/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/ayu-health/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/ayu-health/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/ayu-health/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/ayu-health/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/ayu-health/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "9e3267be7b210606973cc541e3f30791",
+    "url": "/ayu-health/static/media/postimage1.c72d392b.jpg"
+  },
+  {
+    "revision": "2b567d9967f14c6eab92893926dcadd9",
+    "url": "/ayu-health/static/media/postimage2.18b157c7.jpg"
+  },
+  {
+    "revision": "4f733cb5ea1f6d1706decda486c73f2d",
+    "url": "/ayu-health/static/media/postimage3.a1dd241f.jpg"
+  },
+  {
+    "revision": "66cd1a50b2f8063a0560255c11f2786a",
+    "url": "/ayu-health/static/media/postimage4.1f7f03ad.jpg"
+  },
+  {
+    "revision": "33f3bbb0db40500a6c2bf55f0de29748",
+    "url": "/ayu-health/static/media/postimage5.aec7cdaa.jpg"
+  },
+  {
+    "revision": "27084a36a3d767bc07d12e51f46ae660",
+    "url": "/ayu-health/static/media/postimage6.c7af5589.jpg"
+  },
+  {
+    "revision": "9faf7d7efcdfa71a62dd4932a2ffab53",
+    "url": "/ayu-health/static/media/postimage7.3acc293d.jpg"
+  },
+  {
+    "revision": "6922fd2cfb95dc962f1f22a5d44e6f74",
+    "url": "/ayu-health/static/media/postimage8.0618a330.jpg"
+  },
+  {
+    "revision": "8224f618ad667f37b3666a56d20e10e7",
+    "url": "/ayu-health/static/media/postimage9.3ff337e9.jpg"
+  },
+  {
+    "revision": "7d8419a4ad1eb62320f44175d217cb5d",
+    "url": "/ayu-health/static/media/postimage10.2482fad9.jpg"
+  },
+  {
+    "revision": "cae12cd0bd39b8072c12ee40ff43d5ff",
+    "url": "/ayu-health/static/media/postimage11.6f24b227.jpg"
+  },
+  {
+    "revision": "2393b154a75461792008d92235e0c44e",
+    "url": "/ayu-health/static/media/postimage12.e6ff123c.jpg"
+  },
+  {
+    "revision": "9c3092bce66e6a455e20c851c6fe5771",
+    "url": "/ayu-health/static/media/postimage13.0102607b.jpg"
+  },
+  {
+    "revision": "d2f61eb32d889c19755104c87ca671e5",
+    "url": "/ayu-health/static/media/postimage14.1add16bd.jpg"
+  },
+  {
+    "revision": "ac0f6b5428bfc5e75aa5aca7e33c906a",
+    "url": "/ayu-health/static/media/postimage15.d00aa4be.jpg"
+  },
+  {
+    "revision": "955320267bebd0e3995b6628fc438d18",
+    "url": "/ayu-health/static/media/postimage16.71a05b2b.jpg"
+  },
+  {
+    "revision": "dec7ff80625ee6010a6a5b5642001991",
+    "url": "/ayu-health/static/media/profileimage2.f5eeebc3.jpeg"
+  },
+  {
+    "revision": "f3f68bf5b7dce8acae55c25f724625dd",
+    "url": "/ayu-health/expo-service-worker.js"
+  },
+  {
+    "revision": "91e80b696b78af451656c1f8dc4464d1",
+    "url": "/ayu-health/index.html"
+  },
+  {
+    "revision": "a59c803cb064308c0f4556d2ac9171c0",
+    "url": "/ayu-health/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/ayu-health/serve.json"
+  },
+  {
+    "revision": "5e491f7873b4041cbc4cea6b70b3b6cf",
+    "url": "/ayu-health/static/js/2.a0403deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521f8c844d3af13c3d20ba01b2fc5274",
+    "url": "/ayu-health/favicon-16.png"
+  },
+  {
+    "revision": "d50b3a5941cfe755e600a75113771581",
+    "url": "/ayu-health/favicon-32.png"
+  },
+  {
+    "revision": "fa6155203a3b9f3590820927f5a5991a",
+    "url": "/ayu-health/favicon.ico"
+  },
+  {
+    "revision": "d11a5a2499b1cd07ea40359d0b2d5a78",
+    "url": "/ayu-health/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/ayu-health/expo-service-worker.js",{scope:"/ayu-health/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
